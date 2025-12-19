a0c8a89c2144b8eec1288bc9aaae255efd10d10b
Cade McDonald <Cademcdon0@gmail.com>
2025-12-18 23:57:44 -0500

    first draft

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const supabaseClient = require('@supabase/supabase-js');
+const dotenv = require('dotenv');
+
+const app = express();
+const port = 3006;
+dotenv.config();
+
+app.use(bodyParser.json());
+
+//initialize supabase client
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
+
+app.get('/artists', async (req,res) => {
+    console.log('Attempting to GET all artists');
+
+    const {data, error} = await supabase.from('artist').select();
+
+    if (error) {
+        console.log(`Error: ${error}`);
+        res.statusCode = 500;
+        res.send(error);
+    } else {
+        res.send(data);
+    }
+})
+
+app.post('/artist', async (req,res) => {
+    console.log('adding artist...');
+    console.log('Request: ', req.body);
+
+    const artistName = req.body.artistName;
+    const bestSong = req.body.bestSong;
+    const secondBest = req.body.secondBest;
+    const thirdBest = req.body.thirdBest;
+    const authorName = req.body.authorName;
+
+    const {data, error} = await supabase.from('artist').insert({
+        artist_name: artistName,
+        best_song: bestSong,
+        second_best: secondBest,
+        third_best: thirdBest,
+        author_name: authorName,
+    }).select();
+
+    if (error) {
+        console.log(`Error: ${error}`);
+        res.statusCode = 500;
+        res.send(error);
+    } else {
+        res.send(data);
+    }
+    
+    res.send(req.body);
+})
+
+app.listen(port, () => {
+    console.log('App is available on port: ', port);
+})
